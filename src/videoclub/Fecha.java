51997361edaed9f4735a7836dc1d913e461a819a
DAW <luchontandil3@gmail.com>
2019-04-17 20:20:09 +0200

    miguel

--- src/videoclub/Fecha.java
+++ src/videoclub/Fecha.java
@@ -1,50 +1,88 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package videoclub;
 
 import java.util.Calendar;
 
 /**
  *
- * @author DAW
+ * @author Miguel
  */
 public class Fecha {
- 
+    /** 
+     * nuemro entero que hace referencia a un dia
+     * The dia. 
+     */
    private int dia;
+   /** 
+     * nuemro entero que hace referencia a un mes
+     * The mes. 
+     */
    private int mes;
+   /** 
+     * nuemro entero que hace referencia a un anyo
+     * The anyo. 
+     */
    private int anyo;
    
 ////////////////////// GET Y SET \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
+   /**
+    * Gets the  dia.
+    *
+    * @return the dia
+    */
     public int getDia() {
         return dia;
     }
-
+     /**
+    * Sets the dia.
+    *
+    * @param dia the new dia
+    */
     public void setDia(int dia) {
         this.dia = dia;
     }
-
+     /**
+    * Gets the  mes.
+    *
+    * @return the mes
+    */
     public int getMes() {
         return mes;
     }
-
+     /**
+    * Sets the mes.
+    *
+    * @param mes the new mes
+    */
     public void setMes(int mes) {
         this.mes = mes;
     }
-
+     /**
+    * Gets the  anyo.
+    *
+    * @return the anyo
+    */
     public int getAnyo() {
         return anyo;
     }
-
+     /**
+    * Sets the anyo.
+    *
+    * @param anyo the new anyo
+    */
     public void setAnyo(int anyo) {
         this.anyo = anyo;
     }
 
 ////////////////////// CONSTRUCTOR \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
+        /**
+	 * Instantiates a new fecha.
+	 *
+	 * @param dia the dia
+	 * @param mes the mes
+	 * @param anyo the anyo
+	 
+	 */
     public Fecha(int dia, int mes, int anyo) {
         this.dia = dia;
         this.mes = mes;
@@ -54,7 +92,12 @@ public class Fecha {
     
     
  ////////////////////// METODOS. \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-   
+        /**
+	 *sirve para calcular si un año es bisiesto o no, es decir,
+	 *para calcular si febrero tiene 28 o 29 dias 
+	 *duvuelve true si el mes tiene 29 dias y false si tiene 28
+	 *  
+	 */
    public boolean bisiesto(){
         
         if ((this.anyo % 4 == 0) && ((this.anyo % 100 != 0) || (this.anyo % 400 == 0)))
@@ -64,7 +107,14 @@ public class Fecha {
         
         
     }
-   
+        /**
+	 *sirve para calcular si una fecha es valida o no,es decir,
+	 *si los datos metidos al crear la fecha son correctos o no 
+	 * que el dia este entre 1 y 31 y calcule el mes para tener 30, 31, 28 o 29
+         * calcule que el mes esta entre 1 y 12
+         * y que el año este entre 1900 y 2200
+         * devulve un true si la fecha pasada y un false y un cambio de valor a 0 en el campo incorrecto si la fecha no es correcta
+	 */
    public boolean valida(){
         
         boolean fechaOk=true;
@@ -106,11 +156,20 @@ public class Fecha {
                 
     }
     
-    
+        /**
+	 *Muestra la fecha que le pasamos con el siguiente formato 
+	 *dia-mes-año  
+	 */
     public String corta(){        
         return this.dia + "-" + this.mes + "-" + this.anyo;         
     }
-   
+    
+        /**
+	 * Este metodo lo que hace es calcular la diferencia de años que tiene la fecha que le pasas con la fecha actual
+	 * utiliza el calendar para saber a que dia estamos hoy y resta el año con el pasado como parametro
+         * y depsues calcula mes y dias para saber si el año se a cumplido o n. 
+         *
+	 */
     public int calcDif(Fecha fecha){
      int anyos=0;
      Calendar ca = Calendar.getInstance();		
--- src/videoclub/Pelicula.java
+++ src/videoclub/Pelicula.java
@@ -1,74 +1,153 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package videoclub;
 
 /**
  *
- * @author DAW
+ * @author MIGUEL
+ */
+/**
+ * The Class Empleado.
  */
 public class Pelicula {
-    
+    /** 
+     * String de numeros para identificar una pelicula.
+     * The id. 
+     */
     private String id;
+    /** 
+     * Numero entero que permite saber la edad minima recomendada para poder ver la pelicula
+     * The edadMin. 
+     */
     private int edadMin;
+    /** 
+     * Nombre del director de la pelicula
+     * The director. 
+     */
     private String director;
+    /** 
+     * Precio de alquiler de la pelicula
+     * The precio. 
+     */
     private double precio;
+    /** 
+     * Titulo de la pelicula
+     * The titulo. 
+     */
     private String titulo;
+    /** 
+     * Verdaderoo o false para saber si esta disponible una pelicula
+     * The disponible. 
+     */
     private boolean disponible;
 
 ////////////////////// GET Y SET \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-  
+    /**
+    * Gets the  id.
+    *
+    * @return the id
+    */
     public String getId() {
         return id;
     }
-
+    /**
+    * Sets the id.
+    *
+    * @param id the new ID
+    */
     public void setId(String id) {
         this.id = id;
     }
-
+    /**
+    * Gets the EdadMin.
+    *
+    * @return the EdadMin
+    */
     public int getEdadMin() {
         return edadMin;
     }
-
+    /**
+    * Sets the EdadMin.
+    *
+    * @param EdadMin the new EdadMin
+    */
     public void setEdadMin(int edadMin) {
         this.edadMin = edadMin;
     }
-
+    /**
+    * Gets the Director.
+    *
+    * @return the Director
+    */
     public String getDirector() {
         return director;
     }
-
+    /**
+    * Sets the Director.
+    *
+    * @param Director the new Director
+    */
     public void setDirector(String director) {
         this.director = director;
     }
-
+    /**
+    * Gets the Precio.
+    *
+    * @return the Precio
+    */
     public double getPrecio() {
         return precio;
     }
-
+    /**
+    * Sets the Precio.
+    *
+    * @param Precio the new Precio
+    */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
-
+    /**
+    * Gets the Titulo.
+    *
+    * @return the Titulo
+    */
     public String getTitulo() {
         return titulo;
     }
-
+    /**
+    * Sets the Titulo.
+    *
+    * @param Titulo the new Titulo
+    */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
+    /**
+    * Gets the Disponible.
+    *
+    * @return the Disponible
+    */
     public boolean isDisponible() {
         return disponible;
     }
-
+    /**
+    * Sets the Disponible.
+    *
+    * @param Disponible the new Disponible
+    */
     public void setDisponible(boolean disponible) {
         this.disponible = disponible;
     }
 ////////////////////// CONSTRUCTOR \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\   
-
+        /**
+	 * Instantiates a new pelicula.
+	 *
+	 * @param id the id
+	 * @param edadMin the edadMin
+	 * @param director the director
+	 * @param precio the precio
+	 * @param titulo the titulo
+	 * @param disponible the disponible
+	 */
     public Pelicula(String id, int edadMin, String director, double precio, String titulo, boolean disponible) {
         this.id = id;
         this.edadMin = edadMin;
@@ -78,6 +157,11 @@ public class Pelicula {
         this.disponible = disponible;
     }
 
+    /**
+	 * Metodo que devuelve  toda la informacion sobre la pelicula
+	 *
+	 * @return informacion sobre la pelicula: id, edadMin, director, precio, titulo, disponible
+	 */
     @Override
     public String toString() {
         
